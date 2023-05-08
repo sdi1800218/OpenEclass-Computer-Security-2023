694af55219ca39de498333dfd9920566fc0ee00a
Yury Balenciaga Boyka <yury@boyka.ru>
2023-05-08 21:15:04 +0300

    Think I fixed another one

--- openeclass/modules/conference/pass_parameters.php
+++ openeclass/modules/conference/pass_parameters.php
@@ -42,8 +42,8 @@ $fileParameter   = $coursePath.'/'.$currentCourseID.'/.parameters.txt';
 
 if (is_file($fileParameter))
 {
-     	$fp = fopen($fileParameter, 'r')
-                or die ('<center>$langChatError</center>');
+    $fp = fopen($fileParameter, 'r')
+            or die ('<center>$langChatError</center>');
  	$URL = unserialize(fread($fp, filesize($fileParameter)));
 	
 	fclose($fp);
@@ -53,15 +53,15 @@ if ($is_adminOfCourse) {
 
 		if(isset($_POST["video_URL"]))
 		{
-		 	$URL["video_URL"]=stripslashes($_POST["video_URL"]);
+		 	$URL["video_URL"]=htmlspecialchars($_POST["video_URL"]);
 		}
 		if(isset($_POST["presantation_URL"]))
 		{
-			$URL["presantation_URL"]=stripslashes($_POST["presantation_URL"]);
+			$URL["presantation_URL"]=htmlspecialchars($_POST["presantation_URL"]);
 		}
 		if(isset($_POST["netmeeting_show"]))
 		{
-			$URL["netmeeting_show"]=$_POST["netmeeting_show"];
+			$URL["netmeeting_show"]=htmlspecialchars($_POST["netmeeting_show"]);
 		}
 
 		if(isset($_POST["action"]))
@@ -75,25 +75,25 @@ if ($is_adminOfCourse) {
 					unset($URL["presantation_URL"]);
 				}
 		}
-     		$fp = fopen($fileParameter, 'w+');
+     	$fp = fopen($fileParameter, 'w+');
 		fwrite($fp,serialize($URL));
 		fclose($fp);
 }
 
 /*student get parameters for conference module*/
-               if($_POST["variable"]=="netmeeting_show")
-                {
-			if(isset($URL["netmeeting_show"]))
-                       		echo $URL["netmeeting_show"];
-                }
-                if($_POST["variable"]=="video_URL")
-                {
-			if(isset($URL["video_URL"]))
-                       		echo $URL["video_URL"];
-                }
-                if($_POST["variable"]=="presantation_URL")
-                {
-			if(isset($URL["presantation_URL"]))
-                       		echo $URL["presantation_URL"];
-                }
+	if($_POST["variable"]=="netmeeting_show")
+	{
+		if(isset($URL["netmeeting_show"]))
+			echo $URL["netmeeting_show"];
+	}
+	if($_POST["variable"]=="video_URL")
+	{
+		if(isset($URL["video_URL"]))
+			echo $URL["video_URL"];
+	}
+	if($_POST["variable"]=="presantation_URL")
+	{
+		if(isset($URL["presantation_URL"]))
+			echo $URL["presantation_URL"];
+	}
 ?>
